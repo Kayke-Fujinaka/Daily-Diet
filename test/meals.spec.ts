d1ee91420eee803d246311a7cc00a806e5b004ff
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-26 14:51:25 -0300

    test: update meal attributes

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -60,7 +60,7 @@ describe('Meals routes', () => {
     ])
   })
 
-  it('should be able to list all meals', async () => {
+  it('should be able to get specific meal', async () => {
     await request(app.server)
       .post('/meals')
       .send({
@@ -85,6 +85,40 @@ describe('Meals routes', () => {
         name: 'Café da Manhã',
         description: 'Comi uma tapioca e tomei um copo de whey',
         isDiet: 1,
+      }),
+    )
+  })
+
+  it('should be able to edit meal attributes', async () => {
+    await request(app.server)
+      .post('/meals')
+      .send({
+        name: 'Café da Manhã',
+        description: 'Comi uma tapioca e tomei um copo de whey',
+        isDiet: true,
+      })
+      .expect(201)
+
+    const listMealsResponse = await request(app.server)
+      .get('/meals')
+      .expect(200)
+
+    const id = listMealsResponse.body.meals[0].id
+
+    const updateMealResponse = await request(app.server)
+      .put(`/meals/${id}`)
+      .send({
+        name: 'Lanche da Tarde',
+        description: 'Comi um sorvete de chocolate com coca-cola',
+        isDiet: false,
+      })
+      .expect(200)
+
+    expect(updateMealResponse.body.meal).toEqual(
+      expect.objectContaining({
+        name: 'Lanche da Tarde',
+        description: 'Comi um sorvete de chocolate com coca-cola',
+        isDiet: 0,
       }),
     )
   })
