3e707c740fc8cf89da0cef13ffa8af13349481de
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-26 12:35:54 -0300

    config: setup knex

---
+++ src/database.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex'
+import { env } from './env'
+
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
